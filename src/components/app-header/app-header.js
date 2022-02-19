995327ffa88b3bc459d3d6966dae6f1f79b93bfe
i-potashov <i.ptshv@gmail.com>
2022-02-19 23:32:52 +0300

    Initial structure

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import appHeaderStyles from './app-header.module.css';
+
+class AppHeader extends React.Component {
+    render() {
+        return (
+            <></>
+        )
+    }
+}
+
+export default AppHeader;
